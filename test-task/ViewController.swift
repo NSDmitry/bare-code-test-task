f2d2688a6ecef5acc0e0240f644d7b95b14925df
Dmitry Serov <serov.d@rubetek.com>
2018-11-14 16:06:13 +0300

    init project

---
+++ test-task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-task
+//
+//  Created by admin on 14/11/2018.
+//  Copyright © 2018 nsdmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
